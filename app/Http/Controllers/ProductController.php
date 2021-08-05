fd62113492c32332d00a7e36c1663b723b67b82f
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2021-08-05 14:42:38 +0100

    home page slider with products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     //
 
     function index(){
-        return "index";
+        $data=Product::all();
+        return view('product')->with('products',$data);
     }
 }
---
+++ resources/views/product.blade.php
@@ -0,0 +1,31 @@
+@extends('master',['title'=>'Products'])
+
+@section("content")
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" class="custom-product">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+      @foreach($products as $item)
+    <div class=" carousel-item {{$item['id']==1? 'active' : '' }} ">
+      <img class="slider-img" src="{{$item->gallery}}" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block slider-text">
+        <h5>{{$item->name}}</h5>
+        <p>{{$item->description}}</p>
+      </div>
+    </div>
+    @endforeach
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+@endsection
